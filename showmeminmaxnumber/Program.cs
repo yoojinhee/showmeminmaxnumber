18a809d422e842a0858aec68faf736273bae418e
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-12 12:13:53 +0900

    가장 큰 수&작은 수 구하기

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace showmeminmaxnumber
 {
@@ -10,6 +6,33 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[5];
+            int max, min;
+            for(int i=0; i<arr.Length; i++)
+            {
+                Console.Write(i + 1 + ". 숫자를 입력해주세요: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("===============================================");
+
+            min = arr[0];
+            max = arr[0];
+
+            for(int i=1; i<arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+
+            Console.WriteLine("가장 큰 수 : " + max);
+            Console.WriteLine("가장 작은 수 : " + min);
+
         }
     }
 }
